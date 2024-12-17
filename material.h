892fe40b8cd93719845ac9b8ab7c9079ab700386
Adam Viberg <adamvib@kth.se>
2024-12-17 12:32:01 +0100

    Set direction to last value if vec length is 0

--- material.h
+++ material.h
@@ -50,7 +50,13 @@ class lambertian : public material {
         __device__ lambertian(const vec3& a) : albedo(a) {}
         __device__ virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered, curandState *local_rand_state) const  {
              vec3 target = rec.p + rec.normal + random_in_unit_sphere(local_rand_state);
-             scattered = ray(rec.p, target-rec.p);
+
+             vec3 direction = target - rec.p;
+             if (direction.length() < DataType(1e-8)) {
+                 direction = rec.normal;
+             }
+             scattered = ray(rec.p, direction);
+
              attenuation = albedo;
              return true;
         }
