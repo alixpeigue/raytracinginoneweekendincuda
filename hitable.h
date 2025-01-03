19c65a40582c0c49bab642b66ce6468f7910f525
Alix Peigue <irobot.moi@gmail.com>
2025-01-03 16:05:07 +0100

    Use __half for hit

--- hitable.h
+++ hitable.h
@@ -2,20 +2,21 @@
 #define HITABLEH
 
 #include "ray.h"
+#include <cuda_fp16.h>
 
 class material;
 
-struct hit_record
-{
-    float t;
-    vec3 p;
-    vec3 normal;
-    material *mat_ptr;
+struct hit_record {
+  float t;
+  vec3 p;
+  vec3 normal;
+  material *mat_ptr;
 };
 
-class hitable  {
-    public:
-        __device__ virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+class hitable {
+public:
+  __device__ virtual bool hit(const ray &r, __half t_min, __half t_max,
+                              hit_record &rec) const = 0;
 };
 
 #endif
--- sphere.h
+++ sphere.h
@@ -1,44 +1,47 @@
 #ifndef SPHEREH
 #define SPHEREH
 
+#include "cuda_fp16.h"
 #include "hitable.h"
 
-class sphere: public hitable  {
-    public:
-        __device__ sphere() {}
-        __device__ sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
-        __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
-        vec3 center;
-        float radius;
-        material *mat_ptr;
+class sphere : public hitable {
+public:
+  __device__ sphere() {}
+  __device__ sphere(vec3 cen, float r, material *m)
+      : center(cen), radius(r), mat_ptr(m) {};
+  __device__ virtual bool hit(const ray &r, __half tmin, __half tmax,
+                              hit_record &rec) const;
+  vec3 center;
+  float radius;
+  material *mat_ptr;
 };
 
-__device__ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = dot(oc, r.direction());
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - a*c;
-    if (discriminant > 0) {
-        float temp = (-b - sqrt(discriminant))/a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.point_at_parameter(rec.t);
-            rec.normal = (rec.p - center) / radius;
-            rec.mat_ptr = mat_ptr;
-            return true;
-        }
-        temp = (-b + sqrt(discriminant)) / a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.point_at_parameter(rec.t);
-            rec.normal = (rec.p - center) / radius;
-            rec.mat_ptr = mat_ptr;
-            return true;
-        }
+__device__ bool sphere::hit(const ray &r, __half t_min, __half t_max,
+                            hit_record &rec) const {
+  vec3 oc = r.origin() - center;
+  __half a = dot(r.direction(), r.direction());
+  __half b = dot(oc, r.direction());
+  __half c = dot(oc, oc) - radius * radius;
+  __half discriminant = b * b - a * c;
+  if (discriminant > (__half)0.0f) {
+    __half temp = (-b - hsqrt(discriminant)) / a;
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.point_at_parameter(rec.t);
+      rec.normal = (rec.p - center) / radius;
+      rec.mat_ptr = mat_ptr;
+      return true;
     }
-    return false;
+    temp = (-b + hsqrt(discriminant)) / a;
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.point_at_parameter(rec.t);
+      rec.normal = (rec.p - center) / radius;
+      rec.mat_ptr = mat_ptr;
+      return true;
+    }
+  }
+  return false;
 }
-
 
 #endif
