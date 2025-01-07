657f6143d20816054323ff8c0803a0c34a8e4ad7
Jonathan Skantz <jskantz@kth.se>
2025-01-07 15:13:11 +0100

    half to bf16

--- datatype.h
+++ datatype.h
@@ -2,9 +2,9 @@
 #define DATATYPEH
 
 #include <cmath>
-#include "cuda_fp16.h"
+#include "cuda_bf16.h"
 
-using DataType = __half;
+using DataType = __nv_bfloat16;
 
 __host__ __device__ inline DataType float2datatype(float in) {
     return in;
