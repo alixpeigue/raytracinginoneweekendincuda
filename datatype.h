5203fc7e56976c5e7d35c4884b5b6d2de746c286
Jonathan Skantz <jskantz@kth.se>
2025-01-07 15:14:23 +0100

    half to double

--- datatype.h
+++ datatype.h
@@ -2,20 +2,19 @@
 #define DATATYPEH
 
 #include <cmath>
-#include "cuda_fp16.h"
 
-using DataType = __half;
+using DataType = double;
 
 __host__ __device__ inline DataType float2datatype(float in) {
     return in;
 }
 
 __device__  inline DataType datatypesqrt(DataType in) {
-    return hsqrt(in);
+    return sqrt(in);
 }
 
 __device__ inline DataType datatypetan(DataType in) {
-    return hsin(in)/hcos(in);
+    return sin(in)/cos(in);
 }
 
 #endif
